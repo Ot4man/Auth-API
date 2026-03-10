2b272b9fe9f01fe6b6638879482ff79443edc2e8
Ot4man <melloukiotmane@gmail.com>
2026-03-10 12:22:35 +0000

    feat:add controllers auth ,profile controllers

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Requests\RegisterRequest;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+
+class AuthController extends Controller
+{
+
+
+    public function register(RegisterRequest $request)
+    {
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password)
+        ]);
+
+        return response()->json([
+            'message' => 'Account created successfully'
+        ], 201);
+    }
+}
