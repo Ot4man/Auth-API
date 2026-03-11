43667a087b91c56b89665566c7c0e1671e376bdd
Ot4man <melloukiotmane@gmail.com>
2026-03-11 12:13:44 +0000

    update:update the Auth controller

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\http\Requests\LoginRequest;
 use Illuminate\Http\Request;
 use App\Http\Requests\RegisterRequest;
 use Illuminate\Support\Facades\Hash;
@@ -9,7 +10,25 @@ use App\Models\User;
 
 class AuthController extends Controller
 {
+    public function login(LoginRequest $request)
+    {
+        $cred = $request->validated();
 
+        $user = User::where('email', $cred['email'])->first();
+
+        if (!$user || !Hash::check($cred['password'], $user->password)) {
+            return response()->json([
+                'message' => 'Invalid credentials'
+            ], 401);
+        }
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'message' => 'Login successful',
+            'token' => $token
+        ], 200);
+    }
 
     public function register(RegisterRequest $request)
     {
@@ -22,5 +41,13 @@ class AuthController extends Controller
         return response()->json([
             'message' => 'Account created successfully'
         ], 201);
+    }
+
+    public function logout(Request $request)
+    {
+        $request->user()->tokens()->delete();
+        return response()->json([
+            'message' => 'Logout succefly'
+        ], 200);
     }
 }
