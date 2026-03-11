f6f1eed23d996807f0f9019531b67f9a30ac87db
Ot4man <melloukiotmane@gmail.com>
2026-03-11 14:18:03 +0000

    feat:add getprofile methode me()

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -5,8 +5,16 @@ use App\Models\User;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Http\Request;
-
+use App\Http\Requests\UpdateProfileRequest;
 class ProfileController extends Controller
 {
-    //
+    public function me(Request $request)
+{
+    return response()->json([
+        'message' => 'Profile fetched successfully',
+        'data' => $request->user()
+    ], 200);
+}
+
+
 }
